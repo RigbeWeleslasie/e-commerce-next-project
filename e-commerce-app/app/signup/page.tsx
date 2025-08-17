919147f916b79636a5ad6256b190e4eefeb84bb2
Rigbe Weleslasie <rigbewweleslasie@gmail.com>
2025-08-17 19:12:52 +0300

    Updated the signup pages

--- e-commerce-app/app/signup/page.tsx
+++ e-commerce-app/app/signup/page.tsx
@@ -2,16 +2,13 @@ import Image from "next/image";
 import Button from "../SharedComponents/Button";
 import { FaGoogle, FaFacebook, FaHeart, FaSearch, FaShoppingCart } from "react-icons/fa";
 import Link from "next/link";
-import Footer from "../SharedComponents/Footer";
-import Navbar from "../SharedComponents/Navbar";
-import FlashSales from "../components/flashSales";
 export default function Signup() {
   return (
     <div className="min-h-screen bg-white flex flex-col">
     
-      <main className="flex flex-col md:flex-row justify-center items-center flex-1 px-8 py-12 gap-12">
-         <div className="flex flex-1">
-        <div className="w-1/2 flex justify-center items-center bg-[#E3F6F5]">
+      <main className="flex flex-col md:flex-row justify-center items-center flex-1 px-8 py-12 gap-12 ">
+        <div className="flex flex-1">
+        <div className="w-1/2 flex justify-center items-center bg-[#E3F6F5] min-h-[1000px]">
           <Image
             src="/iphone14.png" 
             alt="Shopping Cart"
@@ -20,9 +17,7 @@ export default function Signup() {
             className="object-contain"
           />
         </div>
-      
-
-        <div className="max-w-md w-full">
+        <div className="w-1/2 flex flex-col justify-center px-20 min-h-[1000px]">
           <h2 className="text-2xl font-semibold mb-2">Create an account</h2>
           <p className="text-gray-500 mb-4">Enter your details below</p>
           <form className="flex flex-col gap-4">
@@ -30,7 +25,6 @@ export default function Signup() {
             <input type="email" placeholder="Email or phone number" className="border p-3 rounded-md" required />
             <input type="password" placeholder="Password" className="border p-3 rounded-md" required />
             <Button text="Create Account" type="submit" />
-
             <button
               type="button"
               className="w-full py-3 border rounded-md flex justify-center items-center gap-2"
@@ -38,7 +32,6 @@ export default function Signup() {
               <FaGoogle size={20} />
               Sign up with Google
             </button>
-
             <p className="mt-4">
               Already have an account?{" "}
               <Link href="/login" className="text-blue-500 underline">
